092bea7aa162b163b5dfe839b7a857dfe0afe73f
Lileck <miroshnikovalili@mail.ru>
2025-02-15 06:08:35 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 # https://azinkin.ru/orm.html
 
+
 class Category(models.Model):
     title = models.CharField(
         verbose_name='название',
@@ -17,13 +18,12 @@ class Category(models.Model):
         unique=True,
     )
 
-    def __str__(self):
+    def str(self):
         return self.title
 
     class Meta:
         verbose_name = 'категорию'
         verbose_name_plural = 'категории'
-
 
 class Post(models.Model):
     title = models.CharField(
@@ -48,22 +48,22 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
 
-    def __str__(self):
+    def str(self):
         return self.title
 
     class Meta:
         verbose_name = 'пост'
         verbose_name_plural = 'публикации'
         unique_together = ('category', 'slug')
-        
- class Comment(models.Model):
+
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
 
-    def __str__(self):
+    def str(self):
         return f"Comment by {self.name} on {self.post.title}"
 
     class Meta:
@@ -75,19 +75,19 @@ class Like(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     reaction = models.IntegerField(choices=[(1, 'Лайк'), (-1, 'Дизлайк')])
 
-    def __str__(self):
+    def str(self):
         return f"Like on {self.post.title}"
 
     class Meta:
         verbose_name = "Лайк"
         verbose_name_plural = "Лайки"
 
-class Donat(models.Model):
+class Donation(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField()
-    image = models.ImageField(upload_to='donat/')
+    image = models.ImageField(upload_to='donations/')
 
-    def __str__(self):
+    def str(self):
         return self.title
 
     class Meta:

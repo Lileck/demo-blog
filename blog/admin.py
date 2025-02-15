f5b1bb836bdb901c40f592dd9f42626fbea40a4a
Lileck <miroshnikovalili@mail.ru>
2025-02-15 04:21:17 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Post, Comment, Like, Donation
+from .models import Category, Post, Comment, Like, Donat
 
 admin.site.register(Category)
 admin.site.register(Post)

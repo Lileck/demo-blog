73fa85e8079ee544b5786f3491f2757f466c2e6d
Lileck <miroshnikovalili@mail.ru>
2025-02-15 06:34:09 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+     from .models import Post, Category
+
+     def post_list(request):
+         posts = Post.objects.all()
+         return render(request, 'blog/post_list.html', {'posts': posts})
+
+     def post_detail(request, category_slug, slug):
+         post = get_object_or_404(Post, slug=slug, category__slug=category_slug)
+         return render(request, 'blog/post_detail.html', {'post': post})
 
 # Create your views here.
